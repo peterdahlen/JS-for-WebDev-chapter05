c90390e05471b62d64d37efb703be185d13ee936
Peter Dahlén <peterjandahlen@gmail.com>
2018-02-28 22:13:10 +0100

    5.0
    Löst challenge - adderat ytterliggare adress.

--- 05_07/script.js
+++ 05_07/script.js
@@ -1,30 +1,52 @@
+var gMapAPIKey = 'AIzaSyCxbuSSpadS-nSXOQw39ARTGjvqgnzDmO8';
+
 var map;
 function initMap() {
-	var storeLocation = {lat: 34.1031131, lng: -118.326343};
+	var storeLocationVine = {lat: 34.1031131, lng: -118.326343};
+		storeLocationDodgers = {lat: 34.0745409, lng: -118.2408881}
+		centerPoint = {lat: 34.0937772394951, lng: -118.27888622568359};
 
 	map = new google.maps.Map(document.getElementById('hplus-map'), {
-		'center' : storeLocation,
-		'zoom' : 12,
+		'center' : centerPoint,
+		'zoom' : 11,
 		'mapTypeId' : google.maps.MapTypeId.ROADMAP,
 		'draggable' : false,
 		'scrollwheel' : false
 	});
 	
-	var popupContent = 'H+ Sport Hollywood<br>';
-	    popupContent += '1750 Vine St<br>';
-	    popupContent += 'Los Angeles, CA';
+	var popupContentVine = 'H+ Sport Hollywood<br>';
+	    popupContentVine += '1750 Vine St<br>';
+		popupContentVine += 'Los Angeles, CA';
+		
+	var popupContentDodgers = 'H+ Sport Dodgers Stadium<br>';
+	    popupContentDodgers += '1000 Vin Scully Ave<br>';
+	    popupContentDodgers += 'Los Angeles, CA';
 	
-	var infowindow = new google.maps.InfoWindow({
-		content: popupContent
+	var infowindowVine = new google.maps.InfoWindow({
+		content: popupContentVine
+	});
+
+	var infowindowDodgers = new google.maps.InfoWindow({
+		content: popupContentDodgers
 	});
 	
-	var marker = new google.maps.Marker({
-		'position': storeLocation,
+	var markerVine = new google.maps.Marker({
+		'position': storeLocationVine,
 		'map': map,
-		'title': 'H+ Sport in Hollywood (actually Capitol Records in real life)'
+		'title': 'H+ Sport in Hollywood, Vine Street'
+	});
+
+	var markerDodgers = new google.maps.Marker({
+		'position': storeLocationDodgers,
+		'map': map,
+		'title': 'H+ Sport in Dodger Stadium'
 	});
 	
-	marker.addListener('click', function() {
-		infowindow.open(map, marker);
+	markerVine.addListener('click', function() {
+		infowindowVine.open(map, markerVine);
+	});
+
+	markerDodgers.addListener('click', function() {
+		infowindowDodgers.open(map, markerDodgers);
 	});
 }
