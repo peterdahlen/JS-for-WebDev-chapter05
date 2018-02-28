87f5fd35201122398d1e2e42b0b49c3ea2c4984b
Peter Dahlén <peterjandahlen@gmail.com>
2018-02-28 20:11:21 +0100

    3.0
    Adderat markör på kartan

--- 05_03/script.js
+++ 05_03/script.js
@@ -2,8 +2,18 @@ var gMapAPIKey = 'AIzaSyCxbuSSpadS-nSXOQw39ARTGjvqgnzDmO8';
 
 var map;
 function initMap() {
+	var storeLocation = {lat: 34.1031252, lng: -118.3263873};
 	map = new google.maps.Map(document.getElementById('hplus-map'), {
-		center: {lat: 34.1031252, lng: -118.3263873},
-		zoom: 8
+		'center' : storeLocation,
+		'zoom' : 12,
+		'mapTypeId' : google.maps.MapTypeId.ROADMAP,
+		'draggable' : false,
+		'scrollwheel' : false,
 	});
+
+	var marker = new google.maps.Marker({
+		'position' : storeLocation,
+		'map' : map,
+		'title' : 'H+ Sport in Los Angeles'
+	  });
 }
